06feae4670b208b1b8658536d1b10e9e4a6f0a7e
Denis Dinkov <denisdinkov7@gmail.com>
2023-06-08 02:42:14 +0300

    .

--- 1. Getting Started/gs-01-starting-project/app.js
+++ 1. Getting Started/gs-01-starting-project/app.js
@@ -1,10 +1,33 @@
-const inputEl = document.querySelector('input');
-const button = document.querySelector('button');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data(){
+    return {
+      goals: [],
+      enteredValue: '',
+    }},
+  methods: {
+    addGoal(){
+      this.goals.push(this.enteredValue);
+    }
+  }
+}).mount('#app')
 
-button.addEventListener('click',()=> {
-  let newEl = document.createElement('li');
-  newEl.textContent = inputEl.value;
-  listEl.appendChild(newEl)
-  inputEl.value = ''
-})
+
+
+
+
+
+
+
+
+
+
+// const inputEl = document.querySelector('input');
+// const button = document.querySelector('button');
+// const listEl = document.querySelector('ul');
+
+// button.addEventListener('click',()=> {
+//   let newEl = document.createElement('li');
+//   newEl.textContent = inputEl.value;
+//   listEl.appendChild(newEl)
+//   inputEl.value = ''
+// })
