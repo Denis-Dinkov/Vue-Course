241ead03d90aa268d13094a5e484bc2a15a08afd
Denis Dinkov <denisdinkov7@gmail.com>
2023-06-14 13:38:12 +0300

    .

--- 2. Basics & Core Concepts - DOM Interaction with Vue/basics-01-starting-code/app.js
+++ 2. Basics & Core Concepts - DOM Interaction with Vue/basics-01-starting-code/app.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
   data() {
     return{
-      courseGoal: 'Finish the course and learn Vue',
+      courseGoalA: 'Finish the course and learn Vue',
+      courseGoalB: 'Master Vue',
       vueLink: 'https://vuejs.org/'
     }
+  },
+  methods: {
+     outPutGoal(){
+       num = Math.random();
+      if(num > 0.5){
+        return this.courseGoalA
+      } 
+      return this.courseGoalB
+     }
   }
 })
 
