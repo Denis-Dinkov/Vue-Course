651fb2d0bc770f1ee57455563420cd11af931603
Denis Dinkov <denisdinkov7@gmail.com>
2023-06-15 13:38:51 +0300

    .

--- 7/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-import FriendContact from './components/FriendContact.vue'
-const app = createApp(App)
-app.component('friend-list',FriendContact)
-app.mount('#app');
